8ea930b2b386395d9a9daae5b7ef785c964d8bf6
Conor Schaefer <conor@freedom.press>
2017-03-06 11:05:41 -0800

    TEMPORARY: hardcode development machine in test wrapper
    The testinfra wrapper script now targets the development VM only.
    We can update the script to accept arguments again once we're finished
    porting the development spec tests to testinfra.

--- testinfra/test.sh
+++ testinfra/test.sh
@@ -3,17 +3,20 @@ set -e
 set -x
 
 
-if [[ $# -lt 1 ]]; then
-    echo "Usage: $0 <host>"
-    exit 1
-fi
+# Hard-coding the development VM during migration from Serverspec -> Testinfra.
+#if [[ $# -lt 1 ]]; then
+#    echo "Usage: $0 <host>"
+#    exit 1
+#fi
 
-target_host=$1
-shift 1
+target_host=${1:development}
+#shift 1
 
 testinfra \
     -v \
     --connection ansible \
     --ansible-inventory .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory \
     --hosts "${target_host}" \
+    testinfra/development \
+    testinfra/app-code \
     $@
